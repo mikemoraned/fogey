d4dddc108d8d9fe63acda944c03a5ad36f619cc9
Mike Moran <mike@houseofmoran.com>
2017-04-15 01:15:33 +0200

    output grouped by first reaction month

--- crawl.py
+++ crawl.py
@@ -1,12 +1,18 @@
 import logging
+from collections import defaultdict
 from optparse import OptionParser
 from slacker import Slacker
+import datetime
+import re
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
                     level=logging.INFO)
 
 parser = OptionParser()
-parser.add_option("-t", "--token", dest="token", help="your slack token")
+parser.add_option("-t", "--token", dest="token",
+                  help="your slack token")
+parser.add_option("-l", "--limit", type=int, dest="max_members",
+                  help="maximum number of members")
 
 (options, args) = parser.parse_args()
 
@@ -35,9 +41,26 @@ def oldest_reaction(user_id):
                 slack.reactions.list(user_id, page=page))
     return oldest_ts
 
+
+grouped_by_month = defaultdict(list)
 response = slack.users.list()
+max_members = options.max_members
+members = 0
 for member in response.body["members"]:
-    if not member['deleted'] and not member['is_bot']:
+    if not member['deleted'] \
+            and not member['is_bot'] \
+            and members < max_members:
+        members += 1
         print(member)
         oldest_ts = oldest_reaction(member["id"])
-        print(oldest_ts)
+        if oldest_ts:
+            oldest_datetime = datetime.datetime.fromtimestamp(
+                int(re.sub("\..+", "", oldest_ts)))
+            oldest_month = oldest_datetime.strftime("%Y-%m")
+            grouped_by_month[oldest_month].append(
+                (member["id"], member["name"]))
+
+months = sorted(grouped_by_month.keys())
+for month in months:
+    members = grouped_by_month[month]
+    print("{}: {}, {}".format(month, len(members), members))
